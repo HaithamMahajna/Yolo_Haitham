1e3594b0e30406eea3ab150509813eec62c62be8
HaithamMahajna <haithammahajna98@gmail.com>
2025-05-28 15:12:25 +0300

    update

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -6,7 +6,7 @@ set -e
 echo "Starting deployment..."
 
 cd ~/${REPO_NAME}
-
+git checkout dev
 if [ ! -d "venv" ] && [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
     python3.10 -m venv .venv
