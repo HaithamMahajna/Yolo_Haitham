15d521269081f74cf47faa0b80f1bed9947bb163
HaithamMahajna <haithammahajna98@gmail.com>
2025-05-29 12:07:46 +0300

    adding prometheus

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -27,8 +27,16 @@ sudo systemctl daemon-reload
 sudo systemctl restart yolo.service
 sudo systemctl enable yolo.service
 
+sudo systemctl restart otelcol
+
 if ! systemctl is-active --quiet yolo.service; then
   echo "❌ yolo.service is not running."
   sudo systemctl status yolo.service --no-pager
+  exit 1
+fi
+
+if ! systemctl is-active --quiet otelcol.service; then
+  echo "❌ yolo.service is not running."
+  sudo systemctl status otelcol.service --no-pager
   exit 1
 fi
